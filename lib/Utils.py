e4e0fd65a88c1ceea96ee47bc3013e8f5f4a0928
Dzikril <138376370+mddzikril@users.noreply.github.com>
2025-09-01 18:17:30 +0800

    Configure data reader to define schema and read the csv files

--- lib/Utils.py
+++ lib/Utils.py
@@ -5,8 +5,16 @@ def get_spark_session(env):
     if env == "LOCAL":
         return SparkSession.builder \
             .master("local[2]") \
+            .enableHiveSupport() \
             .getOrCreate()
     else:
         return SparkSession.builder \
             .enableHiveSupport() \
             .getOrCreate()
+
+
+def load_df_csv(spark, datafile):
+    return spark.read \
+        .option("header", "true") \
+        .option("inferSchema", "true") \
+        .csv(datafile)
