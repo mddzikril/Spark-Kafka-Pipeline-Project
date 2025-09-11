be0a2bfbb01c947f8c555ca9ade7137db5d33786
Dzikril <138376370+mddzikril@users.noreply.github.com>
2025-09-11 11:26:00 +0800

    completed data transformation requirements

--- project_main.py
+++ project_main.py
@@ -3,7 +3,8 @@ import sys
 from lib import Utils
 from lib.DataReader import read_accounts, read_party_address, read_party
 from lib.DataTransformations import join_accounts_with_party, join_accounts_party_address, add_event_header, \
-    add_key_header
+    add_key_header, add_payload_header, join_party_and_address, add_party_address_struct, join_party_struct, \
+    save_to_json
 from lib.logger import Log4j
 
 if __name__ == '__main__':
@@ -18,31 +19,33 @@ if __name__ == '__main__':
     spark = Utils.get_spark_session(job_run_env)
     logger = Log4j(spark)
 
+    logger.info("Starting spark program")
     accounts_datafile = "test_data/accounts/account_samples.csv"
     parties_datafile = "test_data/parties/party_samples.csv"
     party_address_datafile = "test_data/party_address/address_samples.csv"
 
+    logger.info("Reading data files")
     accounts_df = read_accounts(spark, accounts_datafile)
     party_df = read_party(spark, parties_datafile)
     party_address_df = read_party_address(spark, party_address_datafile)
 
+    logger.info("Adding event header and key header")
     events_header_accounts_df = add_event_header(accounts_df)
     events_and_key_header_accounts_df = add_key_header(events_header_accounts_df)
 
-    joined_accounts_party_df = join_accounts_with_party(events_and_key_header_accounts_df, party_df)
+    logger.info("Join party with address")
+    joined_party_address = join_party_and_address(party_df, party_address_df)
 
-    events_and_key_header_accounts_df.show()
+    logger.info("Generating party relations struct")
+    struct = add_party_address_struct(joined_party_address)
 
-    logger.info("Num Partitions: " + str(accounts_df.rdd.getNumPartitions()))
+    logger.info("Join party relations struct with accounts df")
+    struct_df = join_party_struct(events_and_key_header_accounts_df, struct)
 
-    events_and_key_header_accounts_df.write \
-        .format("json") \
-        .mode("overwrite") \
-        .option("path", "final_output") \
-        .save()
+    logger.info("Adding payload header")
+    final_df = add_payload_header(struct_df)
 
-    joined_accounts_party_address_df = join_accounts_party_address(joined_accounts_party_df, party_address_df)
+    logger.info("Saving datafile to JSON")
+    save_to_json(final_df)
 
-    #joined_accounts_party_address_df.sort("account_id").show()
-
-    logger.info("Finished creating Spark Session")
+    logger.info("Spark program end")
